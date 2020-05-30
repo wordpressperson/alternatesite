aba1a24dddd1274bed6ca284625d63ed21a9cd0f
Ayokunle Odedere <Ayokunleodedere@panoramicsynergy.co.uk>
2020-05-31 00:40:04 +0100

    minors

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -259,7 +259,7 @@ class Homepage extends Component {
       <div className="row">
         <div className="col-lg-12 text-center">
           <h2 className="section-heading text-uppercase">Contact Us</h2>
-          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h3 className="section-subheading text-muted">We always want to hear from you!</h3>
         </div>
       </div>
       <div className="row">
